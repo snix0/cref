59e929e33f80ed07efac50f989606fbe8c290b1f
Andy Feng <ayfeng@uci.edu>
2021-04-27 22:14:47 -0700

    Add basic command lookup functionality

---
+++ main.py
@@ -0,0 +1,19 @@
+import importlib.resources as pkg_resources
+#from . import templates
+import templates
+import sys
+
+
+def refLookup(ref):
+    template = pkg_resources.read_text(templates, ref)
+    return template
+
+def printUsage():
+    print("Usage: %s [ARG]" % sys.argv[0])
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        printUsage()
+        sys.exit(1)
+    else:
+        print(refLookup(sys.argv[1]))

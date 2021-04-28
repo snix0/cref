10c0dee3b235eb60b66c2e63456846730ddbf396
Andy Feng <ayfeng@uci.edu>
2021-04-27 22:35:20 -0700

    Add basic coloring for output

--- main.py
+++ main.py
@@ -3,18 +3,27 @@ import importlib.resources as pkg_resources
 #from . import templates
 import templates
 import sys
+from colorama import Fore
+from colorama import Style
+import colorama
 
 
 def refLookup(ref):
     template = pkg_resources.read_text(templates, ref)
-    return template
+    res = ""
+    for line in template.split('\n'):
+        res += (Fore.BLUE + line if line.startswith("#") else Fore.YELLOW + line) + "\n"
+    return res
 
 def printUsage():
     print("Usage: %s [ARG]" % sys.argv[0])
 
 if __name__ == '__main__':
+    colorama.init()
+
     if len(sys.argv) < 2:
         printUsage()
         sys.exit(1)
     else:
-        print(refLookup(sys.argv[1]))
+        print("Fetching quick reference for: " + Fore.GREEN + sys.argv[1])
+        print(Fore.BLUE +  refLookup(sys.argv[1]) + Style.RESET_ALL)
